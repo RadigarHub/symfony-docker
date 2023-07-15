7edde64c7a6b8160fd6b0d8539370c51bda28d89
Rafa Díaz <diaz.garcia.rafa@gmail.com>
2023-07-15 16:11:35 +0200

    Add demo test to check phpunit

---
+++ tests/Unit/DemoTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DemoTest extends TestCase
+{
+    public function test_that_true_is_true(): void
+    {
+        self::assertTrue(true);
+    }
+}
