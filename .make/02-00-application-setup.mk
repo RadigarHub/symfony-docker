0a24a4fa7b1dcc0bba0806270b8e92c21f4e6606
Rafa Díaz <diaz.garcia.rafa@gmail.com>
2023-11-21 20:47:05 +0100

    feat: Install and configure doctrine bundle

--- .make/02-00-application-setup.mk
+++ .make/02-00-application-setup.mk
@@ -3,3 +3,7 @@
 .PHONY: composer
 composer: ## Run composer commands. Specify the command e.g. via ARGS="install"
 	$(EXECUTE_IN_APPLICATION_CONTAINER) composer $(ARGS);
+
+.PHONY: setup-db
+setup-db: ## Setup the DB tables
+	$(EXECUTE_IN_APPLICATION_CONTAINER) php bin/console doctrine:database:create $(ARGS);
