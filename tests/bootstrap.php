faf643beae14e300520754366026c215bc406b42
Rafa Díaz <diaz.garcia.rafa@gmail.com>
2025-01-21 19:43:41 +0100

    style: Fix files format

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,26 +10,16 @@ if (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
 
-$APP_DEBUG = (bool)($_SERVER['APP_DEBUG'] ?? false);
+$APP_DEBUG = (bool) ($_SERVER['APP_DEBUG'] ?? false);
 if ($APP_DEBUG) {
     umask(0000);
 }
 
 // Drop test database
-passthru(
-    sprintf(
-        'APP_ENV=test php "%s/../bin/console" doctrine:database:drop --if-exists --force',
-        __DIR__
-    )
-);
+passthru(sprintf('APP_ENV=test php "%s/../bin/console" doctrine:database:drop --if-exists --force', __DIR__));
 
 // Create test database
-passthru(
-    sprintf(
-        'APP_ENV=test php "%s/../bin/console" doctrine:database:create --if-not-exists',
-        __DIR__
-    )
-);
+passthru(sprintf('APP_ENV=test php "%s/../bin/console" doctrine:database:create --if-not-exists', __DIR__));
 
 // Create test database schema
 passthru(sprintf('APP_ENV=test php "%s/../bin/console" doctrine:schema:create', __DIR__));
